aed7311d4b64bf58747ae50380bc02648c39f53e
zfy-zhang <zfy_zang@163.com>
2020-12-06 22:01:09 +0800

    网关服务初次构建代码提交

---
+++ wddily-gateway/src/main/java/com/wddily/WddilyGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.wddily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WddilyGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WddilyGatewayApplication.class, args);
+	}
+
+}
